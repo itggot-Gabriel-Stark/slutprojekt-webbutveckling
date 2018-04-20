f821366fa24565585ac211243cabf95e7b212e10
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-04-20 13:18:38 +0200

    La till basic saker

---
+++ site/minify.rb
@@ -0,0 +1,13 @@
+require 'cssminify'
+require 'uglifier'
+
+def minify(path, pattern, minifier)
+    files = Dir.glob("#{path}#{pattern}")
+    minifier, method = minifier.split('.')
+    minifier = Object.const_get(minifier)
+    files.each do |file|
+        minified =  minifier.send(method, File.read(file))
+        File.write(file, minified)
+    end
+
+end
